8d7632a0b36db69c7d7a7696a7affa773e611a77
yaninandiaz <yaninandiaz@gmail.com>
2024-09-09 16:51:10 -0300

    fix router (all and by id)

--- src/routers/job_router.ts
+++ src/routers/job_router.ts
@@ -5,7 +5,7 @@ import { authorizationHandler } from "../middlewares/authorization_handler";
 const jobsRouter = Router();
 
 jobsRouter.post("/", authorizationHandler, jobController.create);
-jobsRouter.get("/:id", authorizationHandler, jobController.getById);
 jobsRouter.get("/all", authorizationHandler, jobController.getAll);
+jobsRouter.get("/:id", authorizationHandler, jobController.getById);
 
 export default jobsRouter;
